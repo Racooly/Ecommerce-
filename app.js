24a7004fca0a0bc5cf10abab73fece3e107f936d
sandro <sandro@gmail.com>
2023-01-07 23:53:06 +0400

    Finished

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-// making account 
+// user account click 
 const user = document.getElementById('userLogo')
 const regCont = document.getElementById('registerCont')
 
 user.addEventListener('click', ()=>{
     regCont.classList.toggle('active')
+})
+
+//Burger menu
+const Burger = document.getElementById('burger')
+const closeB = document.getElementById('Close')
+const burgerMenu = document.getElementById('burger_menu')
+
+Burger.addEventListener('click', ()=>{
+    burgerMenu.classList.add('activated')
+})
+
+closeB.addEventListener('click', ()=>{
+    burgerMenu.classList.remove('activated')
 })
